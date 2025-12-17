2906a997595bc010c80558a36e52851a1e370d9f
Anass-Azdod <aanass.azd@gmail.com>
2025-12-17 23:09:26 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-let button = document.querySelector(".nav-button");
-let link= document.querySelector(".nav-link");
-
-button.addEventListener("click", ()=>{
-    link.classList.toggle("active");
-});
-
